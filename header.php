385443dcaa8566610b0765953385aa852a005872
Phuc Le <info@phuc.at>
2020-03-04 21:16:02 +0100

    fixed closing a tag

--- header.php
+++ header.php
@@ -70,7 +70,11 @@
                     <?php endif; ?>
 
                     <?php if ( $wtp_description || is_customize_preview() ) : ?>
-                        <p class="site-description"> - <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $wtp_description; /* WPCS: xss ok. */ ?></a</p>
+                        <p class="site-description"> - 
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <?php echo $wtp_description; /* WPCS: xss ok. */ ?>
+                        </a>
+                        </p>
                     <?php endif; ?>
 
                 <?php endif; ?>
