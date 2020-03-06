ee375dbc9718272be11a842ebc3a8cdf1dfe5916
Phuc Le <info@phuc.at>
2020-03-06 17:27:57 +0100

    added style for section

--- js/gutenberg-extension.js
+++ js/gutenberg-extension.js
@@ -1,19 +1,25 @@
 wp.domReady( () => {
-    wp.blocks.registerBlockStyle( 'cgb/block-wtp-plugin-block', {
+    wp.blocks.registerBlockStyle( 'wtp/block', {
         name: 'hero',
         label: 'Hero',
         isDefault: true,
     } );
 
-    wp.blocks.registerBlockStyle( 'cgb/block-wtp-plugin-block', {
+    wp.blocks.registerBlockStyle( 'wtp/block', {
         name: 'mediaobject',
         label: 'Media Object',
         isDefault: false,
     } );
 
-    wp.blocks.registerBlockStyle( 'cgb/block-wtp-plugin-block', {
+    wp.blocks.registerBlockStyle( 'wtp/block', {
         name: 'mediaobject-reverse',
         label: 'Media Object reverse',
+        isDefault: false,
+    } );
+
+    wp.blocks.registerBlockStyle( 'wtp/section', {
+        name: 'grid',
+        label: 'Grid',
         isDefault: false,
     } );
 } );
